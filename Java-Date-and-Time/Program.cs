e7955dd0b0e8a69978ccdd74c90f8b99edfe009d
Manuel Garcia <kiramanuel123@gmail.com>
2021-05-12 02:12:30 -0500

    Agregar archivos de proyecto.

---
+++ Java-Date-and-Time/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Java_Date_and_Time
+{
+    class Program
+    {
+        public static String finDay(int mes,int dia,int año)
+        {
+            DateTime myDT = new DateTime(año, mes, dia, new GregorianCalendar());
+            Calendar myCal = CultureInfo.InvariantCulture.Calendar;
+            myDT = myCal.AddDays(myDT, 5);
+
+            return myCal.GetDayOfWeek(myDT).ToString();
+        }
+        static void Main(string[] args)
+        {
+            finDay(4,3,2002);
+        }
+    }
+}
